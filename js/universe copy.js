70adea75ade8d1d9d7e356b4063efe3a6344e0f3
mccsjs <17766123726@163.com>
2025-10-10 21:30:24 +0800

    Site updated: 2025-10-10 21:30:24

---
+++ js/universe copy.js
@@ -0,0 +1,430 @@
+function dark() {window.requestAnimationFrame=window.requestAnimationFrame||window.mozRequestAnimationFrame||window.webkitRequestAnimationFrame||window.msRequestAnimationFrame;var n,e,i,h,t=.05,s=document.getElementById("universe"),o=!0,a="180,184,240",r="226,225,142",d="226,225,224",c=[];function f(){n=window.innerWidth,e=window.innerHeight,i=.216*n,s.setAttribute("width",n),s.setAttribute("height",e)}function u(){h.clearRect(0,0,n,e);for(var t=c.length,i=0;i<t;i++){var s=c[i];s.move(),s.fadeIn(),s.fadeOut(),s.draw()}}function y(){this.reset=function(){this.giant=m(3),this.comet=!this.giant&&!o&&m(10),this.x=l(0,n-10),this.y=l(0,e),this.r=l(1.1,2.6),this.dx=l(t,6*t)+(this.comet+1-1)*t*l(50,120)+2*t,this.dy=-l(t,6*t)-(this.comet+1-1)*t*l(50,120),this.fadingOut=null,this.fadingIn=!0,this.opacity=0,this.opacityTresh=l(.2,1-.4*(this.comet+1-1)),this.do=l(5e-4,.002)+.001*(this.comet+1-1)},this.fadeIn=function(){this.fadingIn&&(this.fadingIn=!(this.opacity>this.opacityTresh),this.opacity+=this.do)},this.fadeOut=function(){this.fadingOut&&(this.fadingOut=!(this.opacity<0),this.opacity-=this.do/2,(this.x>n||this.y<0)&&(this.fadingOut=!1,this.reset()))},this.draw=function(){if(h.beginPath(),this.giant)h.fillStyle="rgba("+a+","+this.opacity+")",h.arc(this.x,this.y,2,0,2*Math.PI,!1);else if(this.comet){h.fillStyle="rgba("+d+","+this.opacity+")",h.arc(this.x,this.y,1.5,0,2*Math.PI,!1);for(var t=0;t<30;t++)h.fillStyle="rgba("+d+","+(this.opacity-this.opacity/20*t)+")",h.rect(this.x-this.dx/4*t,this.y-this.dy/4*t-2,2,2),h.fill()}else h.fillStyle="rgba("+r+","+this.opacity+")",h.rect(this.x,this.y,this.r,this.r);h.closePath(),h.fill()},this.move=function(){this.x+=this.dx,this.y+=this.dy,!1===this.fadingOut&&this.reset(),(this.x>n-n/4||this.y<0)&&(this.fadingOut=!0)},setTimeout(function(){o=!1},50)}function m(t){return Math.floor(1e3*Math.random())+1<10*t}function l(t,i){return Math.random()*(i-t)+t}f(),window.addEventListener("resize",f,!1),function(){h=s.getContext("2d");for(var t=0;t<i;t++)c[t]=new y,c[t].reset();u()}(),function t(){document.getElementsByTagName('html')[0].getAttribute('data-theme')=='dark'&&u(),window.requestAnimationFrame(t)}()};
+dark()
+
+/* 鼠标*/
+var CURSOR;
+
+Math.lerp = (a, b, n) => (1 - n) * a + n * b;
+
+const getStyle = (el, attr) => {
+    try {
+        return window.getComputedStyle
+            ? window.getComputedStyle(el)[attr]
+            : el.currentStyle[attr];
+    } catch (e) {}
+    return "";
+};
+
+class Cursor {
+    constructor() {
+        this.pos = {curr: null, prev: null};
+        this.pt = [];
+        this.create();
+        this.init();
+        this.render();
+    }
+
+    move(left, top) {
+        this.cursor.style["left"] = `${left}px`;
+        this.cursor.style["top"] = `${top}px`;
+    }
+
+    create() {
+        if (!this.cursor) {
+            this.cursor = document.createElement("div");
+            this.cursor.id = "cursor";
+            this.cursor.classList.add("hidden");
+            document.body.append(this.cursor);
+        }
+
+        var el = document.getElementsByTagName('*');
+        for (let i = 0; i < el.length; i++)
+            if (getStyle(el[i], "cursor") == "pointer")
+                this.pt.push(el[i].outerHTML);
+
+        document.body.appendChild((this.scr = document.createElement("style")));
+        // 这里改变鼠标指针的颜色 由svg生成
+        this.scr.innerHTML = `* {cursor: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8' width='8px' height='8px'><circle cx='4' cy='4' r='4' opacity='1.0' fill='rgb(0, 191, 255)'/></svg>") 4 4, auto}`;
+    }
+
+    refresh() {
+        this.scr.remove();
+        this.cursor.classList.remove("hover");
+        this.cursor.classList.remove("active");
+        this.pos = {curr: null, prev: null};
+        this.pt = [];
+
+        this.create();
+        this.init();
+        this.render();
+    }
+
+    init() {
+        document.onmouseover  = e => this.pt.includes(e.target.outerHTML) && this.cursor.classList.add("hover");
+        document.onmouseout   = e => this.pt.includes(e.target.outerHTML) && this.cursor.classList.remove("hover");
+        document.onmousemove  = e => {(this.pos.curr == null) && this.move(e.clientX - 8, e.clientY - 8); this.pos.curr = {x: e.clientX - 8, y: e.clientY - 8}; this.cursor.classList.remove("hidden");};
+        document.onmouseenter = e => this.cursor.classList.remove("hidden");
+        document.onmouseleave = e => this.cursor.classList.add("hidden");
+        document.onmousedown  = e => this.cursor.classList.add("active");
+        document.onmouseup    = e => this.cursor.classList.remove("active");
+    }
+
+    render() {
+        if (this.pos.prev) {
+            this.pos.prev.x = Math.lerp(this.pos.prev.x, this.pos.curr.x, 0.15);
+            this.pos.prev.y = Math.lerp(this.pos.prev.y, this.pos.curr.y, 0.15);
+            this.move(this.pos.prev.x, this.pos.prev.y);
+        } else {
+            this.pos.prev = this.pos.curr;
+        }
+        requestAnimationFrame(() => this.render());
+    }
+}
+
+(() => {
+    CURSOR = new Cursor();
+    // 需要重新获取列表时，使用 CURSOR.refresh()
+})();
+
+// 如果当前页有评论就执行函数，评论表情放大
+if (document.getElementById('post-comment')) owoBig();
+// 表情放大
+function owoBig() {
+    let flag = 1, // 设置节流阀
+        owo_time = '', // 设置计时器
+        m = 3; // 设置放大倍数
+    // 创建盒子
+    let div = document.createElement('div'),
+        body = document.querySelector('body');
+    // 设置ID
+    div.id = 'owo-big';
+    // 插入盒子
+    body.appendChild(div)
+
+    // 构造observer
+    let observer = new MutationObserver(mutations => {
+
+        for (let i = 0; i < mutations.length; i++) {
+            let dom = mutations[i].addedNodes,
+                owo_body = '';
+            if (dom.length == 2 && dom[1].className == 'OwO-body') owo_body = dom[1];
+            // 如果需要在评论内容中启用此功能请解除下面的注释
+            // else if (dom.length == 1 && dom[0].className == 'tk-comment') owo_body = dom[0];
+            else continue;
+            
+            // 禁用右键（手机端长按会出现右键菜单，为了体验给禁用掉）
+            if (document.body.clientWidth <= 768) owo_body.addEventListener('contextmenu', e => e.preventDefault());
+            // 鼠标移入
+            owo_body.onmouseover = (e) => {
+                    if (flag && e.target.tagName == 'IMG') {
+                        flag = 0;
+                        // 移入300毫秒后显示盒子
+                        owo_time = setTimeout(() => {
+                            let height = e.target.clientHeight * m, // 盒子高 2023-02-16更新
+                                width = e.target.clientWidth * m, // 盒子宽 2023-02-16更新
+                                left = (e.x - e.offsetX) - (width - e.target.clientWidth) / 2, // 盒子与屏幕左边距离 2023-02-16更新
+                                top = e.y - e.offsetY; // 盒子与屏幕顶部距离
+
+                            if ((left + width) > body.clientWidth) left -= ((left + width) - body.clientWidth + 10); // 右边缘检测，防止超出屏幕
+                            if (left < 0) left = 10; // 左边缘检测，防止超出屏幕
+                            // 设置盒子样式
+                            div.style.cssText = `display:flex; height:${height}px; width:${width}px; left:${left}px; top:${top}px;`;
+                            // 在盒子中插入图片
+                            div.innerHTML = `<img src="${e.target.src}">`
+                        }, 300);
+                    }
+                };
+            // 鼠标移出隐藏盒子
+            owo_body.onmouseout = () => { div.style.display = 'none', flag = 1, clearTimeout(owo_time); }
+        }
+
+    })
+    observer.observe(document.getElementById('post-comment'), { subtree: true, childList: true }) // 监听的 元素 和 配置项
+}
+
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功🍬",
+                    message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
+
+//get请求
+$.ajax({
+    type: 'get',
+    url: 'https://apis.map.qq.com/ws/location/v1/ip',
+    data: {
+        key: 'MDKBZ-U7H6J-FBJFY-KD2ON-BDRI5-LIB5L',
+        output: 'jsonp',
+    },
+    dataType: 'jsonp',
+    success: function (res) {
+        ipLoacation = res;
+    }
+})
+function getDistance(e1, n1, e2, n2) {
+    const R = 6371
+    const { sin, cos, asin, PI, hypot } = Math
+    let getPoint = (e, n) => {
+        e *= PI / 180
+        n *= PI / 180
+        return { x: cos(n) * cos(e), y: cos(n) * sin(e), z: sin(n) }
+    }
+
+    let a = getPoint(e1, n1)
+    let b = getPoint(e2, n2)
+    let c = hypot(a.x - b.x, a.y - b.y, a.z - b.z)
+    let r = asin(c / 2) * 2 * R
+    return Math.round(r);
+}
+
+function showWelcome() {
+
+    let dist = getDistance(118.59232127, 32.78868157, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let pos = ipLoacation.result.ad_info.nation;
+    let ip;
+    let posdesc;
+    //根据国家、省份、城市信息自定义欢迎语
+    switch (ipLoacation.result.ad_info.nation) {
+        case "日本":
+            posdesc = "よろしく，一起去看樱花吗";
+            break;
+        case "美国":
+            posdesc = "Let us live in peace!";
+            break;
+        case "英国":
+            posdesc = "想同你一起夜乘伦敦眼";
+            break;
+        case "俄罗斯":
+            posdesc = "干了这瓶伏特加！";
+            break;
+        case "法国":
+            posdesc = "C'est La Vie";
+            break;
+        case "德国":
+            posdesc = "Die Zeit verging im Fluge.";
+            break;
+        case "澳大利亚":
+            posdesc = "一起去大堡礁吧！";
+            break;
+        case "加拿大":
+            posdesc = "拾起一片枫叶赠予你";
+            break;
+        case "中国":
+            pos = ipLoacation.result.ad_info.province + " " + ipLoacation.result.ad_info.city + " " + ipLoacation.result.ad_info.district;
+            ip = ipLoacation.result.ip;
+            switch (ipLoacation.result.ad_info.province) {
+                case "北京市":
+                    posdesc = "北——京——欢迎你~~~";
+                    break;
+                case "天津市":
+                    posdesc = "讲段相声吧。";
+                    break;
+                case "河北省":
+                    posdesc = "山势巍巍成壁垒，天下雄关。铁马金戈由此向，无限江山。";
+                    break;
+                case "山西省":
+                    posdesc = "展开坐具长三尺，已占山河五百余。";
+                    break;
+                case "内蒙古自治区":
+                    posdesc = "天苍苍，野茫茫，风吹草低见牛羊。";
+                    break;
+                case "辽宁省":
+                    posdesc = "我想吃烤鸡架！";
+                    break;
+                case "吉林省":
+                    posdesc = "状元阁就是东北烧烤之王。";
+                    break;
+                case "黑龙江省":
+                    posdesc = "很喜欢哈尔滨大剧院。";
+                    break;
+                case "上海市":
+                    posdesc = "众所周知，中国只有两个城市。";
+                    break;
+                case "江苏省":
+                    switch (ipLoacation.result.ad_info.city) {
+                        case "南京市":
+                            posdesc = "这是我挺想去的城市啦。";
+                            break;
+                        case "苏州市":
+                            posdesc = "上有天堂，下有苏杭。";
+                            break;
+                        default:
+                            posdesc = "散装是必须要散装的。";
+                            break;
+                    }
+                    break;
+                case "浙江省":
+                    posdesc = "东风渐绿西湖柳，雁已还人未南归。";
+                    break;
+                case "河南省":
+                    switch (ipLoacation.result.ad_info.city) {
+                        case "郑州市":
+                            posdesc = "豫州之域，天地之中。";
+                            break;
+                        case "南阳市":
+                            posdesc = "臣本布衣，躬耕于南阳。此南阳非彼南阳！";
+                            break;
+                        case "驻马店市":
+                            posdesc = "峰峰有奇石，石石挟仙气。嵖岈山的花很美哦！";
+                            break;
+                        case "开封市":
+                            posdesc = "刚正不阿包青天。";
+                            break;
+                        case "洛阳市":
+                            posdesc = "洛阳牡丹甲天下。";
+                            break;
+                        default:
+                            posdesc = "可否带我品尝河南烩面啦？";
+                            break;
+                    }
+                    break;
+                case "安徽省":
+                    posdesc = "蚌埠住了，芜湖起飞。";
+                    break;
+                case "福建省":
+                    posdesc = "井邑白云间，岩城远带山。";
+                    break;
+                case "江西省":
+                    posdesc = "落霞与孤鹜齐飞，秋水共长天一色。";
+                    break;
+                case "山东省":
+                    posdesc = "遥望齐州九点烟，一泓海水杯中泻。";
+                    break;
+                case "湖北省":
+                    posdesc = "来碗热干面！";
+                    break;
+                case "湖南省":
+                    posdesc = "74751，长沙斯塔克。";
+                    break;
+                case "广东省":
+                    posdesc = "老板来两斤福建人。";
+                    break;
+                case "广西壮族自治区":
+                    posdesc = "桂林山水甲天下。";
+                    break;
+                case "海南省":
+                    posdesc = "朝观日出逐白浪，夕看云起收霞光。";
+                    break;
+                case "四川省":
+                    posdesc = "康康川妹子。";
+                    break;
+                case "贵州省":
+                    posdesc = "茅台，学生，再塞200。";
+                    break;
+                case "云南省":
+                    posdesc = "玉龙飞舞云缠绕，万仞冰川直耸天。";
+                    break;
+                case "西藏自治区":
+                    posdesc = "躺在茫茫草原上，仰望蓝天。";
+                    break;
+                case "陕西省":
+                    posdesc = "来份臊子面加馍。";
+                    break;
+                case "甘肃省":
+                    posdesc = "羌笛何须怨杨柳，春风不度玉门关。";
+                    break;
+                case "青海省":
+                    posdesc = "牛肉干和老酸奶都好好吃。";
+                    break;
+                case "宁夏回族自治区":
+                    posdesc = "大漠孤烟直，长河落日圆。";
+                    break;
+                case "新疆维吾尔自治区":
+                    posdesc = "驼铃古道丝绸路，胡马犹闻唐汉风。";
+                    break;
+                case "台湾省":
+                    posdesc = "我在这头，大陆在那头。";
+                    break;
+                case "香港特别行政区":
+                    posdesc = "永定贼有残留地鬼嚎，迎击光非岁玉。";
+                    break;
+                case "澳门特别行政区":
+                    posdesc = "性感荷官，在线发牌。";
+                    break;
+                default:
+                    posdesc = "带我去你的城市逛逛吧！";
+                    break;
+            }
+            break;
+        default:
+            posdesc = "带我去你的国家逛逛吧。";
+            break;
+    }
+
+    //根据本地时间切换欢迎语
+    let timeChange;
+    let date = new Date();
+    if (date.getHours() >= 5 && date.getHours() < 11) timeChange = "<span>上午好</span>，一日之计在于晨！";
+    else if (date.getHours() >= 11 && date.getHours() < 13) timeChange = "<span>中午好</span>，该摸鱼吃午饭了。";
+    else if (date.getHours() >= 13 && date.getHours() < 15) timeChange = "<span>下午好</span>，懒懒地睡个午觉吧！";
+    else if (date.getHours() >= 15 && date.getHours() < 16) timeChange = "<span>三点几啦</span>，一起饮茶呀！";
+    else if (date.getHours() >= 16 && date.getHours() < 19) timeChange = "<span>夕阳无限好！</span>";
+    else if (date.getHours() >= 19 && date.getHours() < 24) timeChange = "<span>晚上好</span>，夜生活嗨起来！";
+    else timeChange = "夜深了，早点休息，少熬夜。";
+
+    try {
+        //自定义文本和需要放的位置
+        document.getElementById("welcome-info").innerHTML =
+            `<b><center>🎉 欢迎信息 🎉</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里，当前的IP地址为： <span style="color:var(--theme-color)">${ip}</span>， ${posdesc}</b>`;
+    } catch (err) {
+        // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
+    }
+}
+window.onload = showWelcome;
+// 如果使用了pjax在加上下面这行代码
+document.addEventListener('pjax:complete', showWelcome);
+var now = new Date();
+
+// 分享本页
+function share_() {
+    let url = window.location.origin + window.location.pathname
+    try {
+        // 截取标题
+        var title = document.title;
+        var subTitle = title.endsWith("| mccsjs🍋") ? title.substring(0, title.length - 14) : title;
+        navigator.clipboard.writeText('mccsjs🍋的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎欢迎！🍧🍧');
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "成功复制分享信息🎉",
+                    message: "您现在可以通过粘贴直接跟小伙伴分享了！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success", 
+                    duration: 5000
+                });
+                // return { visible: false }
+            }
+        })
+    } catch (err) {
+        console.error('复制失败！', err);
+    }
+    // new ClipboardJS(".share", { text: function () { return '标题：' + document.title + '\n链接：' + url } });
+    // btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+}
+
+// 防抖
+function share() {
+    debounce(share_, 300);
+}
