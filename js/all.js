5f0ae5ba2f4454716f8167bbd907f9c9ec44ecc6
mccsjs <17766123726@163.com>
2026-02-08 14:03:52 +0800

    Site updated: 2026-02-08 14:03:51

--- js/all.js
+++ js/all.js
@@ -1027,42 +1027,28 @@ document.addEventListener('themechange', addLinkStatusStyles);
 // 初始执行
 initializeFeatures();
 
-// // 快速友链
-// var leonus = {
-//     linkCom: e => {
-//         var t = document.querySelector(".el-textarea__inner");
-//         "bf" == e ? (t.value = "```yml\n", t.value += "- name: \n  link: \n  avatar: \n  descr: \n  siteshot: ", t.value += "\n```", t.setSelectionRange(15, 15)) : (t.value = "站点名称：\n站点地址：\n头像链接：\n站点描述：\n站点截图：", t.setSelectionRange(5, 5)), t.focus()
-//     },
-//     owoBig: () => {
-//         if (!document.getElementById("post-comment") || document.body.clientWidth < 768) return;
-//         let e = 1,
-//             t = "",
-//             o = document.createElement("div"),
-//             n = document.querySelector("body");
-//         o.id = "owo-big", n.appendChild(o), new MutationObserver((l => {
-//             for (let a = 0; a < l.length; a++) {
-//                 let i = l[a].addedNodes,
-//                     s = "";
-//                 if (2 == i.length && "OwO-body" == i[1].className) s = i[1];
-//                 else {
-//                     if (1 != i.length || "tk-comment" != i[0].className) continue;
-//                     s = i[0]
-//                 }
-//                 s.onmouseover = l => {
-//                     e && ("OwO-body" == s.className && "IMG" == l.target.tagName || "tk-owo-emotion" == l.target.className) && (e = 0, t = setTimeout((() => {
-//                         let e = 3 * l.path[0].clientHeight,
-//                             t = 3 * l.path[0].clientWidth,
-//                             a = l.x - l.offsetX - (t - l.path[0].clientWidth) / 2,
-//                             i = l.y - l.offsetY;
-//                         a + t > n.clientWidth && (a -= a + t - n.clientWidth + 10), a < 0 && (a = 10), o.style.cssText = `display:flex; height:${e}px; width:${t}px; left:${a}px; top:${i}px;`, o.innerHTML = `<img src="${l.target.src}">`
-//                     }), 300))
-//                 }, s.onmouseout = () => {
-//                     o.style.display = "none", e = 1, clearTimeout(t)
-//                 }
-//             }
-//         })).observe(document.getElementById("post-comment"), {
-//             subtree: !0,
-//             childList: !0
-//         })
-//     },
-// };
+document.addEventListener('pjax:complete', tonav);
+document.addEventListener('DOMContentLoaded', tonav);
+//响应pjax
+function tonav() {
+    var nameContainer = document.querySelector("#nav #name-container");
+	var menusItems = document.querySelector("#nav .menus_items");
+    var position = $(window).scrollTop();
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll > position + 5) {
+            nameContainer.classList.add("visible");
+            menusItems.classList.remove("visible");
+        } else if  (scroll < position - 5){
+            nameContainer.classList.remove("visible");
+            menusItems.classList.add("visible");
+        }
+
+        position = scroll;
+    });
+
+    // 初始化 page-name
+    document.getElementById("page-name").innerText = document.title.split(" | LiuShen's Blog")[0];
+}
